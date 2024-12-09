26ce2518f403cfc07cd8abc83919835578cdef4d
qkarolayne <137188339+qkarolayne@users.noreply.github.com>
2024-12-08 21:36:20 -0300

    Add files via upload

---
+++ teste_projeto/menu.js
@@ -0,0 +1,8 @@
+function abrir_menu(){
+    document.getElementById("menu_oculto").style.width="300px";
+    document.getElementById("principal").style.marginLeft="300px";
+}
+function fechar_menu(){
+    document.getElementById("menu_oculto").style.width="0";
+    document.getElementById("principal").style.marginLeft="0";
+}
